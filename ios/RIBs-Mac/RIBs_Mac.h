c9b1c3473c9bc9ffe9a614b7271f1635f58c640d
Dmitry Osipa <d.osipa@anchorfree.com>
2019-09-23 17:46:23 -0700

    added separate mac build target (#3)

---
+++ ios/RIBs-Mac/RIBs_Mac.h
@@ -0,0 +1,19 @@
+//
+//  RIBs_Mac.h
+//  RIBs-Mac
+//
+//  Created by Dmitry Osipa on 9/23/19.
+//  Copyright © 2019 Uber Technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RIBs_Mac.
+FOUNDATION_EXPORT double RIBs_MacVersionNumber;
+
+//! Project version string for RIBs_Mac.
+FOUNDATION_EXPORT const unsigned char RIBs_MacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RIBs_Mac/PublicHeader.h>
+
+
